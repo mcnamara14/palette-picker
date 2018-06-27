11150b93599f9c4ca5837e241c6148485eddce55
Tyler McNamara <mcnamara14@gmail.com>
2018-06-27 12:44:44 -0600

    Initial build out of API endpoints

--- server.js
+++ server.js
@@ -1,11 +1,87 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+
+app.locals.projects = [
+	{ id: '1', 
+		name: 'Project 1'
+  },
+  { id: '2', 
+  name: 'Project 2'
+}
+];
+
+app.locals.palettes = [
+  { id: '1', 
+    name: 'Palette 1', 
+    project_id: '1', 
+    colors: [
+        '#E37B40',
+        '#46B29D',
+        '#DE5B49',
+        '#324D5C',
+        '#F0CA4D'
+    ]
+  }
+];
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker';
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(app.get('port'), () => {
     console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
+
+//Get all projects
+
+app.get('/api/v1/projects', (request, response) => {
+	const { projects } = app.locals;
+
+	response.json(projects);
+})
+
+//Get all palettes
+
+app.get('/api/v1/palettes', (request, response) => {
+	const { palettes } = app.locals;
+
+	response.json(palettes);
+})
+
+//Add a project
+
+app.post('/api/v1/projects', (request, response) => {
+	const { name } = request.body;
+	const { projects } = app.locals;
+  const id = Date.now().toString();
+
+	projects.push({ id, name }); 
+})
+
+//Add a palette to a project
+
+app.post('/api/v1/:project_id/palettes', (request, response) => {
+	const { project_id } = request.params;
+	const { palettes } = app.locals;
+	const id = Date.now().toString();
+	const { name, colors } = request.body;
+
+	palettes.push({ id, name, project_id, colors })
+});
+
+//Delete a project
+
+app.delete('/api/v1/projects/:id', (request, response) => {
+  let { projects } = app.locals;
+  const { id } = request.params;
+  const project = projects.find(project => project.id === id);
+
+  project ? app.locals.projects = projects.filter(project => project.id !== id) : null;
+
+  console.log(projects)
+});
+
